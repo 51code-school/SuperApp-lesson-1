24941c9ded5e1019cad229f48086dbf81d02284e
antonhoang <hoang.anton@fintech-farm.com>
2024-07-06 22:02:36 +0200

    initial commit

---
+++ SuperApp/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  SuperApp
+//
+//  Created by Anton Hoang on 03/04/2024.
+//  Copyright © 2024 碼願科技有限公司. All rights reserved.
+
+import SwiftUI
+import Foundation
+
+struct ContentView: View {
+    
+    var body: some View {
+        ZStack {
+            Color.white
+            Text("Welcome to iOS Tigers course!")
+        }
+        .ignoresSafeArea()
+    }
+
+}
+
+#Preview {
+    ContentView()
+}
+
+
+
